0dcf37c6e932279af33633a72d5789b37c8b7293
Jdoggett50 <dillondoggett5@gmail.com>
2023-03-27 23:42:27 -0400

    basic functions created for calculations

---
+++ calculator.js
@@ -0,0 +1,26 @@
+//there will be user input - not sure how we will receive it yet
+
+//add 2 numbers
+function add (num1, num2) {
+    return num1 + num2;
+}   //add(55,3)
+
+//subtract 2 numbers
+function subtract (num1, num2) {
+    return num1 - num2;
+}   //subtract(3,2)
+
+//multiply 2 numbers
+function multiply (num1, num2) {
+    return num1 * num2;
+}   //multiply(2,3)
+
+//divide 2 numbers
+function divide (num1,num2) {
+    return num1 / num2;
+}   //divide(10,2)
+
+//power of
+function powered (num1,num2){
+    return num1 ** num2
+}   //powered(4,2)
