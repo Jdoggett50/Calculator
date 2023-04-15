a6498a084de0fa023b1df76a9351fb21ca162619
Jdoggett50 <dillondoggett5@gmail.com>
2023-04-15 07:15:23 -0400

    git issue-cloned repo. Added Pseudocode comments

--- calculator.js
+++ calculator.js
@@ -1,42 +1,38 @@
 const operators = ['+','-','*','/','=','^'];
 let input1 = '';
 let operator = '';
-
-//input2 = btn.target.value if it's true that input1 exists and operate exists
-//if operator is clicked, update operator variable
+let input2 = '';
 
 const selectionWrapper = document.querySelector('#selection-wrapper')
 selectionWrapper.addEventListener('click', (evt) => {
 	let btnValue = evt.target.value;
-	//clear if false and value = 'AC'
 	console.log(`button clicked is ${numOrOperator(btnValue)}`);
 	console.log('input2 = 3', `operator = ${operator}`);
-	console.log(`result = ${operate(input1,operator,3)}`);	
+	console.log(`result = ${operate(input1,operator,3)}`);
 })
 
 function numOrOperator(btnValue){
 	if(!checkOperator(btnValue) && btnValue == 'AC'){
 		input1 = '0';
-		operator = '0';
+		operator = '';
+		//doesn't exist yet
 		input2 = '0'; 
-		//div element to be set to 0 as well
-	//set input1 as long as value isn't operator
 	} else if(!checkOperator(btnValue)){
-		//return this for placement to input
-		// console.log(`number is true, operator is false: ${input1}`)
 		input1 += btnValue
 		return input1
 	} else if (checkOperator(btnValue)) {
-		//return this for placement to operator
 		operator += btnValue
-		// console.log(`number is false, operator is true: ${operator}`)
 		return operator
-		//console.log(`operator is true: ${operator}`)
 	}
 }
 
+//> if input1 exists and operator exists-
+//  input2 should be assigned value that cannot be another operator 
+//> input2 = btn.target.value if it's true that input1 exists and operate exists
+//> populate result only if '=' after input2
+//> set input1 to be equal to the result of each expression in the case-
+//  of muliple operators after initial expression
 
-//holds initial result value
 function operate (num1, operator, num2) {
 	let expression = [num1,operator,num2];
 	if (expression.includes('+')){
@@ -57,12 +53,11 @@ function checkOperator(btnValue) {
 	let isOperator = false;
 	for(let i = 0; i < operators.length; i++){
 		if(operators[i] == btnValue){
-			//operator = operators[i]
 			return isOperator = operators[i];
 		}
 	}
 	return isOperator
-} //console.log(checkOperator(input))
+}
 
 //placed in operate to evaluate expressions
 function add (num1, num2) {
