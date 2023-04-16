9d2a51b0402c89d42c088608e23558d31248784e
Jdoggett50 <dillondoggett5@gmail.com>
2023-04-16 06:33:39 -0400

    began result evaluator implementation

--- calculator.js
+++ calculator.js
@@ -2,35 +2,37 @@ const operators = ['+','-','*','/','=','^'];
 let input1 = '';
 let operator = '';
 let input2 = '';
-const selectionWrapper = document.querySelector('#selection-wrapper')
+let evaluator = '';
 
-selectionWrapper.addEventListener('click', (evt) => {
+const btns = document.querySelector('#selection-wrapper')
+
+btns.addEventListener('click', (evt) => {
 	console.log(`button clicked is ${updateInputs(evt.target.value)}`);
-	console.log(`input1 = ${input1}`,`operator = ${operator}`, `input2 = ${input2}`);
-	console.log(`result = ${operate(input1,operator,input2)}`);
+	console.log(`input1 = ${input1}`,`operator = ${operator}`, `input2 = ${input2}`,`evaluator = ${evaluator}`);
+	console.log(`result = ${updateInputs(btnVal)}`);
+	getResult()
 })
 
 function updateInputs(btnVal){
-	if(!checkOperator(btnVal) && btnVal == 'AC'){
-		input1 = '';
+	if(btnVal == 'AC'){
+		input1 = '0';
 		operator = '';
-		input2 = '';
+		input2 = '0';
+		evaluator = '';
 	} else if(!checkOperator(btnVal) && operator == ''){
-		input1 += btnVal
-		return input1
-	} else if (checkOperator(btnVal)){
-		operator += btnVal
-		return operator
-	} else if (input1 != '' && operator != ''){
-		input2 += btnVal
-		console.log(input2)
-		return input2
+		input1 += btnVal;
+		return input1;
+	} else if (checkOperator(btnVal) && evaluator == ''){
+		operator += btnVal;
+		return operator;
+	} else if (input1 != '' && operator != '' && evaluator == ''){
+		input2 += btnVal;
+		return input2;
+	} else if (checkOperator(btnVal) && operator != ''){
+		
 	}
 }
 
-//> if input1 exists and operator exists-
-//  input2 should be assigned value that cannot be another operator 
-//> input2 = btn.target.value if it's true that input1 exists and operate exists
 //> populate result only if '=' after input2
 //> set input1 to be equal to the result of each expression in the case-
 //  of muliple operators after initial expression
