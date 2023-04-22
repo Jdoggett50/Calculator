7849f6c4ef35abbb8e4cbaab28db0399e58439b6
Jdoggett50 <dillondoggett5@gmail.com>
2023-04-22 07:07:28 -0400

    buttons add input to result pane

--- calculator.js
+++ calculator.js
@@ -5,41 +5,41 @@ let input2 = '';
 let evaluator = '';
 
 const btns = document.querySelector('#selection-wrapper')
+const resultBox = document.querySelector('.result > p')
+
 
 btns.addEventListener('click', (evt) => {
-	console.log(`button clicked is ${updateInputs(evt.target.value)}`);
-	console.log(`input1 = ${input1}`,`operator = ${operator}`, `input2 = ${input2}`,`evaluator = ${evaluator}`);
-	console.log(`result = ${(getResults(evt.target.value))}`);
+	resultBox.textContent = `${updateInputs(evt.target.value)}`
+	
 })
 
-//evaluator needs to set the operator when all values are in their fields.
 function updateInputs(btnVal){
 	if(btnVal == 'AC'){
 		input1 = '';
 		operator = '';
 		input2 = '';
 		evaluator = '';
+		return result = 0;
 	} else if (checkOperator(btnVal) && operator && input1 && input2){
-		console.log('A')
 		evaluator = btnVal;
 		input1 = getResults();
 		input2 = '';
-	}
-	else if (!checkOperator(btnVal) && !operator){
-		console.log('B')
+		return input1
+	} else if (!checkOperator(btnVal) && !operator){
 		input1 += btnVal;
+		return input1
 	} else if (evaluator && evaluator != '='){
-		console.log('C')
 		input2 += btnVal
 		operator = evaluator;
 		evaluator = '';
+		return input2
 	} else if (!checkOperator(btnVal) && input1 && operator){
-		console.log('D')
 		evaluator = '';
 		input2 += btnVal
+		return input2
 	} else if (checkOperator(btnVal) != '=' && input1){
-	 	console.log('E')
 	 	operator = btnVal;
+		return operator
 	}
 }
 
@@ -47,14 +47,9 @@ function getResults(){
 	if (evaluator){
 		result = operate(input1,operator,input2);
 		return result;
-	} 
+	}
 }
 
-//  of muliple operators after initial expression
-// result should be 0 unless 1 input is placed and submitted.
-// if (input2 != 0) } input2 = btnValue 
-
-//maybe refactor with a class?
 function operate (num1, operator, num2) {
 	let expression = [num1,operator,num2];
 	if (expression.includes('+')){
@@ -70,7 +65,6 @@ function operate (num1, operator, num2) {
 	return result
 }
 
-//returns operator or false
 function checkOperator(btnVal) {
 	let isOperator = false;
 	for(let i = 0; i < operators.length; i++){
@@ -81,7 +75,6 @@ function checkOperator(btnVal) {
 	return isOperator
 }
 
-//placed in operate to evaluate expressions
 function add (num1, num2) {
     return parseInt(num1) + parseInt(num2);
 }
