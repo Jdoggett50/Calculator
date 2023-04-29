3ff9f55a0b1c488b6a6ad63f21e2742074683b66
Jdoggett50 <dillondoggett5@gmail.com>
2023-04-29 05:20:12 -0400

    added support for enter button

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
-const operators = ['+','-','*','/','=','^'];
-const acceptedKeys = ['1','2','3','4','5','6','7','8','9','0','+','-','*','/','=','.'];
+const operators = ['+','-','*','/','=','^','Enter','BackSpace','Escape'];
+const acceptedKeys = ['1','2','3','4','5','6','7','8','9','0','+','-','*','/','=','.','Enter','Backspace','Escape'];
 let input1 = '';
 let operator = '';
 let input2 = '';
@@ -9,85 +9,73 @@ const btns = document.querySelector('#selection-wrapper')
 const resultBox = document.querySelector('.result > p');
 
 btns.addEventListener('click', evt => {
-	console.log(`input1: ${input1} operator: ${operator} input2: ${input2} evaluator: ${evaluator} `)
 	if (evt.target.classList.contains('button')){
-		resultBox.textContent = `${updateInputs(evt.target.value)}`
+		resultBox.textContent = `${updateInputs(evt.target.value)}`;
 	}
-	console.log(`input1: ${input1} operator: ${operator} input2: ${input2} evaluator: ${evaluator}`)
+	console.log(`input1: ${input1} operator: ${operator} input2: ${input2} evaluator: ${evaluator} result: ${getResults()}`);
 });
 
 document.addEventListener('keydown', (evt) => {
 	if (acceptedKeys.includes(evt.key)){ 
-		resultBox.textContent = `${updateInputs(evt.key)}`
+		resultBox.textContent = `${updateInputs(evt.key)}`;
 	}
+	console.log(`input1: ${input1} operator: ${operator} input2: ${input2} evaluator: ${evaluator} result: ${getResults()}`);
 });
 
 function updateInputs(btnVal){
 	if(btnVal == 'AC' || (input1 == '0' && operator =='/' && input2 == '0')){
-		console.log('A')
+		console.log('A');
 		input1 = '';
 		operator = '';
 		input2 = '';
 		evaluator = '';
-		return result = 0;
+		return result = '0';
 	} else if (!acceptedKeys.includes(btnVal)){
-		console.log('B')
+		console.log('B');
 		return '';
 	} else if (operators.includes(btnVal) && operator && input1 && input2){
-		console.log('C')
+		console.log('C');
 		evaluator = btnVal;
 		input1 = getResults();
 		input2 = '';
 		return input1;
 	} else if (!operators.includes(btnVal) && !operator){
-		console.log('D')
+		console.log('D');
 		input1 += checkDecimal(btnVal);
 		return input1;
-	} else if (evaluator && evaluator != '='){
-		console.log('E')
+	} else if (evaluator && evaluator != '=' && evaluator != 'Enter' && evaluator != 'Backspace'){
+		console.log('E');
 		input2 += checkDecimal(btnVal);
 		operator = evaluator;
 		evaluator = '';
 		return input2;
-	} else if (!operators.includes(btnVal) && input1 && operator){
-		console.log('F')
+	} else if (!operators.includes(btnVal) && input1 && operator && btnVal != 'BackSpace'){
+		console.log('F');
 		evaluator = '';
 		input2 += checkDecimal(btnVal);
 		return input2;
 	} else if (operators.includes(btnVal) != '=' && input1){
-		console.log('G')
+		console.log('G');
 		operator = btnVal;
 		return operator;
 	} else
-		console.log('H')
+		console.log('H');
 		return input1 = '';
 } 
 
 function getResults(){
-	if (evaluator){
-		result = operate(operator)
-		return result
-	}
-}
-//works for decimals
-function checkDecimal (btnVal){
-	if((btnVal == '.' && input1.includes('.') && !input2) || (btnVal == '.' && input2.includes ('.')) || (operators.includes(btnVal))){
-		return ''
+	if ((evaluator == '=' || evaluator == 'Enter') || (evaluator != '=' || evaluator != 'Enter')){
+		//result
+		return operate(operator);
 	} else 
-	return btnVal
+		return '';
 }
 
-function add (num1, num2) {
-    return parseFloat(num1) + parseFloat(num2);
-}
-function subtract (num1, num2) {
-    return num1 - num2;
-}
-function multiply (num1, num2) {
-    return num1 * num2;
-}
-function divide (num1,num2) {
-    return num1 / num2;
+function checkDecimal (btnVal){
+	if((btnVal == '.' && input1.includes('.') && !input2) || (btnVal == '.' && input2.includes ('.')) || (operators.includes(btnVal))){
+		return '';
+	} else 
+	return btnVal;
 }
 
 function operate(op) {
@@ -106,14 +94,25 @@ function operate(op) {
 	}
 	return result
 }
-// add clear single input feature 
-// add evaluate on enter feature
-// add decimal support
 
-// possible solutions:
-//  return nothing when a decimal is pressed and input1 or input2 includes a decimal
+function add (num1, num2) {
+    return parseFloat(num1) + parseFloat(num2);
+}
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+function divide (num1,num2) {
+    return num1 / num2;
+}
+
+// add clear single input feature 
+// add decimal support- decimal support under construction
 
 //decimal constraints:
-// first iteration works with decimal, second iteration works with decimal,
-// assignment from evaluator to input1 works, assignment to input2 works as long as-
-// the input isn't a decimal
+// everytime evaluator is received from getResults, it immediately triggers the function call
+
+// possible solutions:
+//  
